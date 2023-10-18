6eb283257b708ff825bdeb676b27c42befed8f5c
Jono Pike <jono.pike@assistdigital.com>
2023-10-18 12:49:54 +0000

    Restoring Snowflake package

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -7,12 +7,12 @@ with payments as (
 pivoted as (
     select
         order_id
-        {% for payment_method in payment_methods -%}
+        ,{% for payment_method in payment_methods -%}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
         {% if not loop.last -%}
         ,
         {%- endif -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
-        {% endfor -%}
+        {%- endfor %}
     from payments
     where status = 'success'
     group by 1
